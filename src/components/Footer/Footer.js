65e5b04da5743d7f7619effd4bbf963eb319070d
Lahchhsin <nishchhalbhardwaj94@gmail.com>
2020-01-30 23:48:09 +0530

    Anchor tag with css:bookmark:

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ class Footer extends Component{
         return(
             <div>
                 <footer className="footer">
-                    <h5>all rights!?@nickb94</h5>
+                    <h5><a href="https://github.com/nickb94/precipe">@nickb94</a></h5>
                     
 
                 </footer>
